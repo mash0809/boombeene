78eaf55ebe6412e73df315402a7b096f103f2c98
mash0809 <tonem0809@gmail.com>
2026-06-25 22:18:10 +0900

    test: 애플리케이션 컨텍스트 테스트 격리

--- src/test/java/com/tonem/boombeene/BoombeeneApplicationTests.java
+++ src/test/java/com/tonem/boombeene/BoombeeneApplicationTests.java
@@ -1,10 +1,13 @@
 package com.tonem.boombeene;
 
+import com.tonem.boombeene.support.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest
-class BoombeeneApplicationTests {
+@DirtiesContext
+class BoombeeneApplicationTests extends AbstractIntegrationTest {
 
 	@Test
 	void contextLoads() {
