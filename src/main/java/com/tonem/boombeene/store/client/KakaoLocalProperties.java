6446fabf541c34eaa505fbc26eb8d0fad4447288
mash0809 <tonem0809@gmail.com>
2026-06-26 00:32:08 +0900

    fix: 카카오 클라이언트 타임아웃 설정

--- src/main/java/com/tonem/boombeene/store/client/KakaoLocalProperties.java
+++ src/main/java/com/tonem/boombeene/store/client/KakaoLocalProperties.java
@@ -2,6 +2,8 @@ package com.tonem.boombeene.store.client;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.time.Duration;
+
 @ConfigurationProperties(prefix = "kakao.local")
-public record KakaoLocalProperties(String baseUrl, String apiKey) {
+public record KakaoLocalProperties(String baseUrl, String apiKey, Duration connectTimeout, Duration readTimeout) {
 }
