2bb054c7546a4dc1addb3e304ee77ec976844e82
mash0809 <tonem0809@gmail.com>
2026-06-25 09:25:32 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/tonem/boombeene/BoombeeneApplication.java
@@ -0,0 +1,13 @@
+package com.tonem.boombeene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoombeeneApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(BoombeeneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tonem/boombeene/BoombeeneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonem.boombeene;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoombeeneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
