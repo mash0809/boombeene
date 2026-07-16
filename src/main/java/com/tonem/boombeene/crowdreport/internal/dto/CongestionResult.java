f861ab1820a246179038fd2f994e53b39cc351e8
mash0809 <tonem0809@gmail.com>
2026-07-16 11:24:43 +0900

    feat: 혼잡도 코멘트 DTO 계약 추가

--- src/main/java/com/tonem/boombeene/crowdreport/internal/dto/CongestionResult.java
+++ src/main/java/com/tonem/boombeene/crowdreport/internal/dto/CongestionResult.java
@@ -1,14 +1,20 @@
 package com.tonem.boombeene.crowdreport.internal.dto;
 
 import com.tonem.boombeene.crowdreport.internal.entity.CongestionLevel;
+import java.util.List;
 
-public record CongestionResult(CongestionLevel level, int count, double distanceMeters) {
+public record CongestionResult(CongestionLevel level, int count, double distanceMeters, List<String> comments) {
 
     public static CongestionResult none(double distanceMeters) {
-        return new CongestionResult(null, 0, distanceMeters);
+        return new CongestionResult(null, 0, distanceMeters, List.of());
     }
 
-    public static CongestionResult of(CongestionLevel level, int count, double distanceMeters) {
-        return new CongestionResult(level, count, distanceMeters);
+    public static CongestionResult of(
+            CongestionLevel level,
+            int count,
+            double distanceMeters,
+            List<String> comments
+    ) {
+        return new CongestionResult(level, count, distanceMeters, comments);
     }
 }
